b46e51b04fe48d8c411037f75adebccd74bbb357
Victor Moraes <dt.victor@gmail.com>
2017-12-11 17:31:30 -0200

    Playing with request logging

---
+++ src/test/java/loggingexample/TwitterRequestLoggingExample.java
@@ -0,0 +1,49 @@
+package loggingexample;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import resources.PropertiesFileHandler;
+
+import java.util.Properties;
+
+import static io.restassured.RestAssured.given;
+
+public class TwitterRequestLoggingExample {
+
+    private String consumerKey;
+    private String consumerSecret;
+    private String accessToken;
+    private String accessSecret;
+
+    @BeforeClass
+    public void setup() {
+        RestAssured.baseURI = "https://api.twitter.com";
+        RestAssured.basePath = "/1.1/statuses";
+        Properties prop = new Properties();
+        PropertiesFileHandler.loadPropertiesFile(prop,"src/test/java/resources/config.properties");
+
+        consumerKey= prop.getProperty("twitter_api_key");
+        consumerSecret= prop.getProperty("twitter_api_secret");
+        accessToken= prop.getProperty("twitter_access_token");
+        accessSecret= prop.getProperty("twitter_access_token_secret");
+    }
+
+    @Test
+    public void requestLogging() {
+        given()
+            .log()
+            //.headers()
+            //.body()
+            //.parameters()
+            //.all()
+            .ifValidationFails()
+            .auth()
+            .oauth(consumerKey,consumerSecret,accessToken,accessSecret)
+            .queryParam("status", "Tweet generated using RestAssured API with Java")
+        .when()
+            .post("/update.json")
+        .then()
+            .statusCode(200);
+    }
+}
